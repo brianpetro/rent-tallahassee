818dceaf086b01910865d7c6167a070501fa0eea
Brian <brian@07gj-DROID.(none)>
2013-01-30 18:30:18 -0500

    add twitter js for tweet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,6 @@
 //= require jquery-ui
 //= require jquery_ujs
 //= require_tree .
+
+// For Twitter
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
