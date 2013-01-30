c621f6c10608dd1264f9c3b4a5c13018c41af978
Brian <brian@07gj-DROID.(none)>
2013-01-30 11:31:41 -0500

    rake db:migrate after classified scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130130074358) do
+ActiveRecord::Schema.define(:version => 20130130162944) do
+
+  create_table "classifieds", :force => true do |t|
+    t.string   "headline"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "roles", :force => true do |t|
     t.string   "name"
