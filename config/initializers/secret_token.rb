51de0efdddfb1a6f1d0b3cbf5549fdf6a2e4f455
Brian <brian@07gj-DROID.(none)>
2013-01-30 02:57:30 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClassifiedSite::Application.config.secret_token = '224ad6a9f7d6da0c545e598cdea08bc2c469220c522170b7c7034fb81728e06127a6a02287841961af68b7b2bff78031a17b1bfd3124b185e3b853a28703df7a'
