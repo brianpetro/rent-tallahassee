0d91f2623a604150faf21ef10aa082c05c47af12
Brian <brian@07gj-DROID.(none)>
2013-01-31 15:17:33 -0500

    make user_id attr accessible

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -1,5 +1,5 @@
 class Classified < ActiveRecord::Base
-  attr_accessible :content, :headline, :email
+  attr_accessible :content, :headline, :email, :user_id
   has_many :subscribers
   accepts_nested_attributes_for :subscribers
   has_many :feed_entries
