3922a70d32d74a72b703e1dec3afb1cdaa4d2d01
Brian <brian@07gj-DROID.(none)>
2013-01-31 02:50:04 -0500

    fragiley parsing xml and posting

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -1,3 +1,21 @@
 class FeedEntry < ActiveRecord::Base
   attr_accessible :guid, :name, :published_at, :summary, :url
+  
+
+	def self.update_from_feed(feed_url)
+		feed = Feedzirra::Feed.fetch_and_parse(feed_url)
+		feed.entries.each do |entry|
+		  unless exists? :guid => entry.id
+		    create!(
+		      :name         => entry.title,
+		      :summary      => entry.summary,
+		      :url          => entry.url,
+		      :published_at => entry.published,
+		      :guid         => entry.entry_id
+		    )
+		  end
+		end
+	end
+
+
 end
