c4937e067f77d03f880b5a93d3513a3cf25e1147
Brian <brian@07gj-DROID.(none)>
2013-01-31 03:14:57 -0500

    has many feed entries

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -2,5 +2,6 @@ class Classified < ActiveRecord::Base
   attr_accessible :content, :headline, :email
   has_many :subscribers
   accepts_nested_attributes_for :subscribers
+  has_many :feed_entries
   
 end
