071bd2315eeff690d35758bb893037c0cfda53d5
Brian <brian@07gj-DROID.(none)>
2013-02-02 17:01:18 -0500

    rake sitemap and diagram

--- sitemap.rb
+++ sitemap.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = 'http://example.com'
+SitemapGenerator::Sitemap.default_host = 'http://www.Rent-Tallahassee.com'
 SitemapGenerator::Sitemap.create do
-  add '/home', :changefreq => 'daily', :priority => 0.9
-  add '/contact_us', :changefreq => 'weekly'
+  add '/', :changefreq => 'daily', :priority => 0.9
+  add '/packages', :changefreq => 'weekly'
 end
 SitemapGenerator::Sitemap.ping_search_engines # called for you when you use the rake task
