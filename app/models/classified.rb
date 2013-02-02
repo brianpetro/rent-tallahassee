f0f131273d2361c7000852335287a9b82894a713
Brian <brian@07gj-DROID.(none)>
2013-02-01 20:56:55 -0500

    move contents to sites model

--- app/models/classified.rb
+++ app/models/classified.rb
@@ -1,8 +1,6 @@
 class Classified < ActiveRecord::Base
   attr_accessible :content, :headline, :email, :user_id
-  has_many :subscribers
-  accepts_nested_attributes_for :subscribers
-  has_many :feed_entries
+
   
   def to_param
   	"#{id}-#{headline}".parameterize
