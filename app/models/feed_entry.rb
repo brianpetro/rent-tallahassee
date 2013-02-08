e54664d9bb6588c0dfa93942f731a27a89b6bff9
Brian <brian@07gj-DROID.(none)>
2013-02-07 21:46:37 -0500

    add site id to upodate from feeds

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -2,7 +2,7 @@ class FeedEntry < ActiveRecord::Base
   attr_accessible :guid, :name, :published_at, :summary, :url
   
 
-	def self.update_from_feed(feed_url)
+	def self.update_from_feed(site, feed_url)
 		feed = Feedzirra::Feed.fetch_and_parse(feed_url)
 		feed.entries.each do |entry|
 		  unless exists? :guid => entry.id
@@ -11,7 +11,8 @@ class FeedEntry < ActiveRecord::Base
 		      :summary      => entry.summary,
 		      :url          => entry.url,
 		      :published_at => entry.published,
-		      :guid         => entry.entry_id
+		      :guid         => entry.entry_id,
+		      :site_id      => site
 		    )
 		  end
 		end
