d119bf603fe26954c58c71e8de0cf762cc09e36b
Brian <brian@07gj-DROID.(none)>
2013-02-07 21:55:53 -0500

    chane site input var to site_id

--- app/models/feed_entry.rb
+++ app/models/feed_entry.rb
@@ -2,7 +2,7 @@ class FeedEntry < ActiveRecord::Base
   attr_accessible :guid, :name, :published_at, :summary, :url
   
 
-	def self.update_from_feed(site, feed_url)
+	def self.update_from_feed(site_id, feed_url)
 		feed = Feedzirra::Feed.fetch_and_parse(feed_url)
 		feed.entries.each do |entry|
 		  unless exists? :guid => entry.id
@@ -12,7 +12,7 @@ class FeedEntry < ActiveRecord::Base
 		      :url          => entry.url,
 		      :published_at => entry.published,
 		      :guid         => entry.entry_id,
-		      :site_id      => site
+		      :site_id      => site_id
 		    )
 		  end
 		end
