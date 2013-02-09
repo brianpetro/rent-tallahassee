3532993c0197509b556832d94d11351463a65d78
Brian <brian@07gj-DROID.(none)>
2013-02-08 21:21:20 -0500

    make site_id attr accesible

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,5 +1,5 @@
 class Subscriber < ActiveRecord::Base
-  attr_accessible :email
+  attr_accessible :email, :site_id
   validates_format_of :email, with: /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
   validates_uniqueness_of :email
 end
