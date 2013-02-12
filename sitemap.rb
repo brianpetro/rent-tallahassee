d814558d1d5cd4971bb0cada8c3e33b52871effd
Brian <brian@07gj-DROID.(none)>
2013-02-12 00:02:10 -0500

    Only root and sitemap

--- sitemap.rb
+++ sitemap.rb
@@ -4,7 +4,6 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'http://www.Rent-Tallahassee.com'
 SitemapGenerator::Sitemap.create do
   add '/', :changefreq => 'daily', :priority => 0.9
-  add '/packages', :changefreq => 'weekly'
   add '/sites/sitemap.xml', changefreq: 'daily', priority: 1
 end
 SitemapGenerator::Sitemap.ping_search_engines # called for you when you use the rake task
